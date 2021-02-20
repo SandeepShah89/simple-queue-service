5ce9ef419f201d35f2ef2f4858b1f1f51c839e42
Sandeep Shah <sandeep89@gmail.com>
2021-02-20 01:09:56 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,27 +3,7 @@
     
     User stories to fulfill:
 
-    1. Identification - TAG
-    - As users enter the site, they'll have tokens in their URLs. We want to preserve these in a cookie, as their UUID.
-
-    2. Redirection service - TAG, SERVICE
-    - After each main-stage event, users get kicked out into the lobby. We need to:
-    -- Present some kind of holding message?                - TAG
-    -- Look up their ID, and:                               - SERVICE
-    -- Allocate Leaders based on pre-defined mapping.       - SERVICE
-    -- Allocate users on a front-fill basis.                - SERVICE
-    - Return URL of where they should go.                   - SERVICE
-    - Tag should pick up this URL and bounce them along.    - TAG
-
-    Notes:
-    - Allocation should be done based on event. Each event has a different set of allocations.
-    // - Leaders should be looked up for pairings and entry before being allocated anywhere. NOT RELEVANT ANYMORE IF WE'RE ALLOCATING BASED ON STRICT MAPPINGS.
-
-    3. On the fly definition of leader tokens - SERVICE 
-    - WW should be able to provide leader tokens on the fly, and handle pairings.
-
-    4. Monitoring of which tokens are in which rooms - SERVICE 
-    - Should be able to pull the lists and provide monitoring at all times.
+    1. When users hit /enter, we work out which event they belong to based on time of day, 
 
 */
 
@@ -34,9 +14,9 @@ let fs = require('fs');
 let util = require('util');
 let debugMode = false;
 
-let req_file = require('fs').createWriteStream(__dirname + '/requests.txt', {flags : 'a'});
-let log_file = require('fs').createWriteStream(__dirname + '/log.txt', {flags : 'a'});
-let err_file = require('fs').createWriteStream(__dirname + '/errors.txt', {flags : 'a'});
+let req_file = fs.createWriteStream(__dirname + '/requests.txt', {flags : 'a'});
+let log_file = fs.createWriteStream(__dirname + '/log.txt', {flags : 'a'});
+let err_file = fs.createWriteStream(__dirname + '/errors.txt', {flags : 'a'});
 
 (function(){
     let old = process.stdout.write;
@@ -68,83 +48,95 @@ app.use((req, res, next) => {
     next();
 });
 
-const port = 3000;
+const port = 80;
 
 let config = {
-    'e1': {
-        numberOfRooms: 75,
-        roomURLs: [
-            // ...roomURLs
-            "room-url-0",
-            "room-url-1",
-            "room-url-2",
-            "room-url-3",
-            "room-url-4"
-        ]
-    },
-
-    'e2': {
-        numberOfRooms: 40,
-        delegatesPerRoom: 5,
-        roomURLs: [
-            // ...roomURLs
-            "room-url-0",
-            "room-url-1",
-            "room-url-2",
-            "room-url-3",
-            "room-url-4"
-        ],
-        leaderAllocation: {
-            // token: roomIndex
-            "leader-1": 0,
-            "leader-2": 0,
-            "leader-3": 1,
-            "leader-4": 1
-        }
-    }
-};
-
-let allocation = {
-    /* 
     e1: {
-        last: N,
+        lastRoom: -1,
         rooms: [
-            ...rooms as [...tokens]
+            '550-090-099',
+            '284-869-116',
+            '556-398-013',
+            '685-855-244',
+            '849-883-863',
+            '135-631-640',
+            '396-211-528',
+            '177-510-464',
+            '415-715-758',
+            '436-492-167',
+            '202-707-096',
+            '143-148-894',
+            '307-375-231',
+            '549-063-767',
+            '684-930-078',
+            '315-167-491',
+            '776-600-344',
+            '219-702-766',
+            '941-406-000',
+            '370-887-601',
+            '277-846-441',
+            '704-474-483',
+            '280-800-015',
+            '465-533-446',
+            '550-741-371',
+            '464-600-589',
+            '328-526-600',
+            '558-840-745',
+            '211-418-658',
+            '709-394-464',
+            '631-151-773',
+            '274-094-669',
+            '434-990-949',
+            '230-686-534',
+            '397-560-220',
+            '557-293-691',
+            '948-825-982',
+            '501-640-781',
+            '970-381-166',
+            '529-099-236',
+            '418-419-555',
+            '694-431-015',
+            '955-284-888',
+            '563-494-405',
+            '466-490-918',
+            '221-565-573',
+            '895-867-485',
+            '814-794-501',
+            '633-218-235',
+            '733-982-222',
+            '123-661-941',
+            '570-211-217',
+            '962-475-404',
+            '851-677-317',
+            '550-560-365',
+            '601-780-522',
+            '523-813-665',
+            '324-169-211',
+            '547-445-463',
+            '103-375-307',
+            '202-712-227',
+            '412-511-256',
+            '515-910-780',
+            '585-948-918',
+            '169-503-919',
+            '340-643-638',
+            '838-075-911',
+            '234-979-776',
+            '149-377-048',
+            '281-921-721',
+            '612-822-054',
+            '636-588-602',
+            '137-581-198',
+            '119-460-442',
+            '678-882-406'
         ]
     },
-
     e2: {
-        curRoom: 0,
-        rooms: {
-            leaders: [...tokens],
-            delegates: [...tokens]
-        }
-    }
-    */
-};
-
-for(let key in config){
-    let nOfRooms = config[key].numberOfRooms;
-
-    if(key == "e1"){
-        allocation[key] = {
-            last: -1,
-            rooms: (new Array( nOfRooms )).fill([]),
-            userAllocation: {}
-        };
-    } else if(key == "e2") {
-
-        allocation[key] = {
-            curRoom: 0,
-            rooms: {
-                leaders: (new Array( nOfRooms )).fill([]),
-                delegates: (new Array( nOfRooms )).fill([])
-            },
-            userAllocation: {}
-        };
-
+        lastRoom: -1
     }
 };
+
+config.e2.rooms = config.e1.rooms.slice(0, 40);
 
 let Setting_Event = false;
 
@@ -219,160 +211,42 @@ app.get('/enter', (req, res) => {
 
     console.log('\n\n/enter');
 
-    let uuid = req.query.token || req.cookies["wto.visitor"];
     let event = Setting_Event || getEvent();
 
     if(!["1", "2"].includes(event)){
         res.json({
-            message: "main stage"
+            room: "901-252-752",
+            roomType: 'main stage'
         });
         return;
     }
 
     console.log(`Event is: ${event}`);
 
-    if(event == "1"){
-        let o = allocation.e1;
-        let URL = false;
-
-        // Check if user has been previously allocated. If they have, just return that.
-        let previousAllocation = o.userAllocation[ uuid ];
-
-        console.log("Previous allocation for uuid: ", uuid, previousAllocation);
-        if(previousAllocation !== undefined){
-            
-            // User's been in this room before:
-            URL = config.e1.roomURLs[ previousAllocation ];
-            console.log("User assigned back to pre-allocated room# ", previousAllocation, URL);
-
-        } else {
-            
-            // New allocation. Round-robin ALL users.
-
-            o.nextIndex++; // starts at -1. increment 
-            if(!o.rooms[ o.nextIndex ]) o.nextIndex = 0; // if we've gone past the last room, go back to 0
-            
-            // Store the allocation, so we can look up who's in which room
-            o.rooms[ o.nextIndex ].push( uuid );
-
-            // Preserve allocation in hashmap too. Easy lookup for users going back to the same room.
-            o.userAllocation[ uuid ] = o.nextIndex; 
-    
-            URL = config.e1.roomURLs[ o.nextIndex ];
-            console.log("User assigned to new room# ", o.nextIndex, URL);
-        }
-        
-        res.json({
-            room: URL,
-            ts: Date.now()
-        });
-
-    } else if(event == "2"){
-
-        let o = allocation.e2;
-        let URL = false;
-
-        // check userAllocation 
-        let previousAllocation = o.userAllocation[ uuid ];
-
-        console.log("Previous allocation for uuid: ", uuid, previousAllocation);
-        if(previousAllocation !== undefined){
-            
-            // User's been in this room before:
-            URL = config.e2.roomURLs[ previousAllocation ];
-            console.log("User assigned back to pre-allocated room# ", previousAllocation, URL);
-
-        } else {
-
-            console.log("Assigning new room. Starting with leader allocation check");
-
-            // Check leader allocation
-            let leaderAlloc = config.e2.leaderAllocation[ uuid ];
-            console.log("Leader allocation: "+leaderAlloc);
-
-            if(leaderAlloc){
-    
-                // If they're a leader, they'll have pre-allocated rooms. Just send them in there.
-                URL = config.e2.roomURLs[ leaderAlloc ];
-            
-                // Also add the user to our room list 
-                o.rooms.leaders[ leaderAlloc ].push(uuid);
-
-                // And to the quick lookup table
-                o.userAllocation[ uuid ] = leaderAlloc;
-    
-            } else {
-    
-                // Not a leader 
-                // Fill-first approach - throw N users into each room. Start to finish.
-
-                let curRoom = o.rooms.delegates[ o.curRoom ];
-                if(curRoom.length < config.e2.delegatesPerRoom){
-
-                    // Add user to room 
-                    curRoom.push( uuid );
-
-                    // Add user to map 
-                    o.userAllocation[ uuid ] = o.curRoom;
-
-                    // Return room 
-                    URL = config.e2.roomURLs [o.curRoom];
-
-                } else {
-
-                    o.curRoom++;
-                    curRoom = o.rooms.delegates[ o.curRoom ];
-                    
-                    // If we hit the limit on rooms, i.e. excessive delegates, go back to 0. 
-                    // We'll keep hitting this condition probably.
-                    if(!curRoom){
-                        o.curRoom = 0;
-                        curRoom = o.rooms.delegates[ o.curRoom ];
-                    }
-
-                    // Add user to room 
-                    curRoom.push( uuid );
-
-                    // Add user to map 
-                    o.userAllocation[ uuid ] = o.curRoom;
-
-                    // Return room 
-                    URL = config.e2.roomURLs [o.curRoom];
-
-
-                }
-
-            }
-
-        }
-
-        if(URL){
-            
-            // Return room
-            res.json({
-                room: URL,
-                ts: Date.now()
-            });
-
-        } else {
-
-            // Return room
-            res.json({
-                error: "No rooms found",
-                ts: Date.now()
-            });
-
-        }
-
+    let thisEvent = config["e"+event];
+    thisEvent.lastRoom++;
+    let roomID = thisEvent.rooms[ thisEvent.lastRoom ];
+    if(!roomID){
+        thisEvent.lastRoom = 0;
+        roomID = thisEvent.rooms[ thisEvent.lastRoom ];
     }
+
+    res.json({
+        room: roomID,
+        roomType: 'breakout'
+    });
+
 });
 
-app.get('/getRooms', function(req, res, next){
-    res.json(allocation); // some front end to call this endpoint and do something creative with the data.
+app.get('/clearAll', (req, res) => {
+    config.e1.lastRoom = -1;
+    config.e2.lastRoom = -1;
 });
-
-// TODO: Nicer output for who's in which room 
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+// 2nd breakout session = first 40 rooms 
+// we don't need to worry about leaders for e2 
+
